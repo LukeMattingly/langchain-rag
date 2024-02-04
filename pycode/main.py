7555080a8b50355dd0b057f751ecbb0ecf0f9cb3
Kycool13 <mattingly.luke@yahoo.com>
2024-02-04 15:54:07 -0700

    setup sequential chains for coding and subsequent testing

--- pycode/main.py
+++ pycode/main.py
@@ -1,6 +1,7 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
+
 import argparse
 from dotenv import load_dotenv
 
@@ -19,11 +20,32 @@ code_prompt = PromptTemplate(
     
 )
 
-code_chain = LLMChain(
-    llm=llm, 
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code:\n {code}",
+    input_variables=["code"]
 )
 
-result = code_chain.run(language=args.language, task=args.task)
+code_chain = LLMChain(
+    llm=llm, 
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain], 
+    input_variables=["language","task"], 
+    output_variables=["code","test"]
+)
+
+result = chain({
+    "language":args.language, 
+    "task":args.task
+})
 
 print(result)
