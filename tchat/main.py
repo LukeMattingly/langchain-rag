92018a499cf3b003fbcb5d8beaad09dbf5f02a4c
Kycool13 <mattingly.luke@yahoo.com>
2024-02-05 22:35:56 -0700

    updated to have a chat conversation

--- tchat/main.py
+++ tchat/main.py
@@ -1,10 +1,34 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
 
+load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
+prompt = ChatPromptTemplate(
+    input_variables = ["content", "messages"],
+    messages = [
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory
+)
 
 
 while True:
     try:
         content = input(">> ")
-        print("you entered: ", content)
-            
+        result = chain({"content":content})
+        print(result['text'])    
     except:
         break
