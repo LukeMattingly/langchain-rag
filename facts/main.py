94b9572168c0604c513cb940b3b1f94bde49e1d3
Kycool13 <mattingly.luke@yahoo.com>
2024-02-06 22:43:10 -0700

    add chunking

--- facts/main.py
+++ facts/main.py
@@ -1,9 +1,19 @@
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 
 loader = TextLoader("facts.txt")
 
-docs = loader.load()   
-print(docs)
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200, 
+    chunk_overlap=0
+)
+
+docs = loader.load_and_split(text_splitter)   
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
