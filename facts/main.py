29024b6c79ddac89a314e20dbc5560bf1e70abcd
Kycool13 <mattingly.luke@yahoo.com>
2024-02-06 23:12:53 -0700

    storing initial text chunk vectors in chroma

--- facts/main.py
+++ facts/main.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
 
 loader = TextLoader("facts.txt")
 
@@ -14,6 +18,12 @@ text_splitter = CharacterTextSplitter(
 
 docs = loader.load_and_split(text_splitter)   
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(docs, 
+                           embeddings, 
+                           persist_directory="emb")
+
+results = db.similarity_search("what is an interesting fact about the english language", k=2)
+
+for result in results:
     print("\n")
+    print(result.page_content)
