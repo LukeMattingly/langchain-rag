638dfdb6dd2d80201d597d3c2471892c5d11e8a3
Kycool13 <mattingly.luke@yahoo.com>
2024-02-10 17:47:20 -0700

    added memory between agent queries

--- agents/main.py
+++ agents/main.py
@@ -2,6 +2,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder)
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
@@ -21,10 +22,13 @@ prompt = ChatPromptTemplate(
                        "Do not make any assumptions about what tables exist "
                        "or what columns exit. Instead, use the 'describe_tables' function"
                        )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
@@ -37,7 +41,12 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory,
 )
 
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file")
+#agent_executor("Summarize the top 5 most popular products. Write the results to a report file")
+
+agent_executor("How many orders are there? Write the result to a html report.")
+
+agent_executor("Repeat the exact same process for users")
