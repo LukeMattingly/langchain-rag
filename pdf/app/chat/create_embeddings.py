34488388d31f091b5162413eb2b92ba6b3291e42
Kycool13 <mattingly.luke@yahoo.com>
2024-02-12 22:28:56 -0700

    create embeddings loader and split

--- pdf/app/chat/create_embeddings.py
+++ pdf/app/chat/create_embeddings.py
@@ -1,3 +1,6 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     """
     Generate and store embeddings for the given pdf
@@ -14,5 +17,14 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
 
     create_embeddings_for_pdf('123456', '/path/to/pdf')
     """
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load_and_split(text_splitter)
+    print(documents)
 
     pass
