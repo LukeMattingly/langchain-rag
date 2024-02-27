dcd25d2fe22d1a8367fc5764c0401db0f460a5c1
Kycool13 <mattingly.luke@yahoo.com>
2024-02-26 19:37:02 -0700

    upload doc, read text, break into chunks

--- pdf/app/chat/create_embeddings.py
+++ pdf/app/chat/create_embeddings.py
@@ -1,3 +1,8 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import sys
+import io
+
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     """
     Generate and store embeddings for the given pdf
@@ -14,5 +19,18 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
 
     create_embeddings_for_pdf('123456', '/path/to/pdf')
     """
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    loader = PyPDFLoader(pdf_path)
+    docs = loader.load_and_split(text_splitter)
+
+    #if you have issue with the below printing, then can try this 
+    #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8') 
+    
+    for doc in docs:
+        try:
+            print(doc.page_content)
+        except UnicodeEncodeError:
+            print("UnicodeEncodeError")
+            
 
     pass
