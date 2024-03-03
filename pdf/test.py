688ad301dffd87a6b8ef16bceb6a2fa04144ad32
Kycool13 <mattingly.luke@yahoo.com>
2024-03-03 14:43:23 -0700

    migrated work out of test.py, setup retrieval and streaming classes

--- pdf/test.py
+++ pdf/test.py
@@ -1,39 +1,27 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
-from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
-from queue import Queue
-from threading import Thread
+
 
 
 load_dotenv()
 
-queue = Queue()
 
-class StreamingHandler(BaseCallbackHandler):
-    def on_llm_new_token(self, token, **kwargs):
-        queue.put(token)
-
-    def on_llm_end(self, response, **kwargs):
-        queue.put(None)
-
-    def on_llm_error(self, error, **kwargs):
-        queue.put(None)
-
-chat = ChatOpenAI(
-    streaming=True,
-    callbacks=[StreamingHandler()]
+chat = ChatOpenAI(streaming=True
 )
 
 prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
 ])
 
-class StreamingChain(LLMChain):
+class StreamableChain:
     def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
+
         def task():
-            self(input)
+            self(input, callbacks=[handler]) #running the chain, using it's own handler
 
         Thread(target=task).start()
         
@@ -42,6 +30,9 @@ class StreamingChain(LLMChain):
             if token is None:
                 break
             yield token
+
+class StreamingChain(StreamableChain, LLMChain):
+    pass
 
 chain = StreamingChain(llm=chat, prompt=prompt)
 
