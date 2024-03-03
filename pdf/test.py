fa04beb47b0e18b6bf2463a23f76c526c7d02783
Kycool13 <mattingly.luke@yahoo.com>
2024-03-03 13:38:28 -0700

    current version of test.py non functional

--- pdf/test.py
+++ pdf/test.py
@@ -1,15 +1,42 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
 
 load_dotenv()
 
-chat = ChatOpenAI(streaming=True)
+queue = Queue()
+
+class StreamingHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs):
+        queue.put(token)
+
+    def on_llm_end(self, response: str, **kwargs):
+        queue.put(None)
+
+chat = ChatOpenAI(streaming=True, callbacks=[StreamingHandler()])
 
 prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
 ])
 
-messages = prompt.format_messages(content="What is the meaning of life?")
-for message in chat.stream(messages):
-    print(message.content)
+class StreamingChain(LLMChain):
+    def stream(self, input):
+        def task():
+            self(input)
+
+        Thread(target=task).start()
+
+        while not queue.empty():
+            token = queue.get()
+            if token is None:
+                break
+            yield token
+
+chain = StreamingChain(llm=chat, prompt=prompt)
+
+for output in chain.stream(input = {"content":"tell me a joke"}):
+    print(output)
