4990f44fce08bb599fd5d704558ca18c84a10752
Kycool13 <mattingly.luke@yahoo.com>
2024-03-05 23:52:08 -0700

    text streaming is working!

--- pdf/app/chat/chat.py
+++ pdf/app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
@@ -17,10 +18,12 @@ def build_chat(chat_args: ChatArgs):
     """
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever,
     )
